04d18201dadf3a14dd654bcc58e23b3ec2004dbd
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-03-31 10:14:00 +0200

    fix dark tweets

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,10 @@
-window.onload = function () {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        let tweets = document.getElementsByClassName("twitter-tweet");
-        for (const tweet of tweets) {
-            tweet.setAttribute("data-theme", "dark")
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "interactive") {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            let tweets = document.getElementsByClassName("twitter-tweet");
+            for (const tweet of tweets) {
+                tweet.setAttribute("data-theme", "dark")
+            }
         }
     }
-};
+});
