2161b1a3d7bdcb9acf336a53bbb0fa218be32952
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-11-21 16:20:38 +0100

    return color scheme toggle

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,7 @@
 document.addEventListener('readystatechange', event => {
     if (event.target.readyState === "interactive") {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches ||
+            document.body.classList.contains("colorscheme-dark")) {
             let tweets = document.getElementsByClassName("twitter-tweet");
             for (const tweet of tweets) {
                 tweet.setAttribute("data-theme", "dark")
